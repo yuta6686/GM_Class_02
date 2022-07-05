1c03c9add6200f717b8f406d5c81eb4f98ce1f76
yuta6686 <yuta20010614@gmail.com>
2022-07-05 09:39:55 +0900

    UI_Chargeの中心座標を変更：x->左が0.0f,y->そのまま Diffuse左右で変えた。 溜めの演出logで表現

--- UI_Charge.cpp
+++ UI_Charge.cpp
@@ -1,32 +1,33 @@
 #include "UI_Charge.h"
 #include "ResourceManager.h"
+#include <cmath>
 
 void UI_Charge::Init()
 {
-	m_mainPos.x = 100.0f;
-	m_mainPos.y = 25.0f;
+	m_mainPos.x = MAIN_POS_X;
+	m_mainPos.y = MAIN_POS_Y;
 
-	m_Radius = 100.0f;
+	m_Radius = sqrtf(m_mainPos.x * m_mainPos.x + m_mainPos.y * m_mainPos.y);
 
 
-	m_vertex[0].Position = D3DXVECTOR3(-m_mainPos.x, -m_mainPos.y, 0.0f);
+	m_vertex[0].Position = D3DXVECTOR3(0.0f, -m_mainPos.y, 0.0f);
+	m_vertex[1].Position = D3DXVECTOR3(m_mainPos.x * 2.0f, -m_mainPos.y, 0.0f);
+	m_vertex[2].Position = D3DXVECTOR3(0.0f, m_mainPos.y, 0.0f);
+	m_vertex[3].Position = D3DXVECTOR3(m_mainPos.x * 2.0f, m_mainPos.y, 0.0f);
+
 	m_vertex[0].Normal = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	m_vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	m_vertex[0].TexCoord = D3DXVECTOR2(0.0f, 0.0f);
-
-	m_vertex[1].Position = D3DXVECTOR3(m_mainPos.x, -m_mainPos.y, 0.0f);
 	m_vertex[1].Normal = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	m_vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	m_vertex[1].TexCoord = D3DXVECTOR2(1.0f, 0.0f);
-
-	m_vertex[2].Position = D3DXVECTOR3(-m_mainPos.x, m_mainPos.y, 0.0f);
 	m_vertex[2].Normal = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	m_vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	m_vertex[2].TexCoord = D3DXVECTOR2(0.0f, 1.0f);
-
-	m_vertex[3].Position = D3DXVECTOR3(m_mainPos.x, m_mainPos.y, 0.0f);
 	m_vertex[3].Normal = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	m_vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+
+	m_vertex[0].Diffuse = D3DXVECTOR4(1.0f, 0.0f, 0.0f, 1.0f);	//	左上
+	m_vertex[2].Diffuse = D3DXVECTOR4(1.0f, 0.0f, 0.0f, 1.0f);	//	左下
+	m_vertex[1].Diffuse = D3DXVECTOR4(0.0f, 1.0f, 1.0f, 1.0f);	//	右上
+	m_vertex[3].Diffuse = D3DXVECTOR4(0.0f, 1.0f, 1.0f, 1.0f);	//	右下
+
+	m_vertex[0].TexCoord = D3DXVECTOR2(0.0f, 0.0f);
+	m_vertex[1].TexCoord = D3DXVECTOR2(1.0f, 0.0f);
+	m_vertex[2].TexCoord = D3DXVECTOR2(0.0f, 1.0f);
 	m_vertex[3].TexCoord = D3DXVECTOR2(1.0f, 1.0f);
 
 	//	頂点バッファ生成
@@ -55,11 +56,13 @@ void UI_Charge::Init()
 	m_PixelShader = ResourceManger<PixelShader>::GetResource(PixelShader::UNLIT_PIXEL_SHADER.c_str());
 
 	m_Position = { 0.0f,0.0f,0.0f };
-	m_Offset.x = m_mainPos.x;
+	m_Offset.x = 0.0f;
 	m_Offset.y = SCREEN_HEIGHT - m_mainPos.y;
 	m_Position += m_Offset;
 	m_Rotation = { 0.0f,0.0f,0.0f };
 	m_Scale = { 1.0f,1.0f,1.0f };
+
+	m_Counter = 1.0f;
 }
 
 void UI_Charge::Uninit()
@@ -71,7 +74,15 @@ void UI_Charge::Update()
 {
 	UserInterface::Update();
 
-	m_Position = m_mainPos;
+	m_Position.y = m_mainPos.y;
+	
+	if (m_Counter >= COUNTER_MAX) {
+		m_Counter = 1.0f;
+	}
+	m_Counter+= CHARGE_SPEED;
+	m_Scale.x = logf(m_Counter);
+
+
 }
 
 void UI_Charge::Draw()
--- UI_Charge.h
+++ UI_Charge.h
@@ -3,6 +3,12 @@
 class UI_Charge :
     public UserInterface
 {
+private:
+    inline static const float COUNTER_MAX = 123.0f;
+    inline static const float CHARGE_SPEED = 0.5f;
+    inline static const float MAIN_POS_X = 100.0f;
+    inline static const float MAIN_POS_Y = 20.0f;
+    float m_Counter = 1.0f;
 public:
     virtual void Init() override;
     virtual void Uninit() override;
@@ -11,4 +17,5 @@ public:
 
     
 };
+
 
