3f779c08be7a4eac3d5fbee81518355bdb979173
yuta6686 <yuta20010614@gmail.com>
2022-11-13 18:15:30 +0900

    一旦PBRは出来たけど、変な線出てる。

--- MeshFieldPS.hlsl
+++ MeshFieldPS.hlsl
@@ -10,18 +10,82 @@ SamplerState g_SamplerState : register(s0);
 static const float PI = 3.1415926f;
 
 // 関数
-//float3 GetNormal(float3 normal, float3 tangent, float3 biNormal, float2 uv)
-//{
+float3 GetNormal(float3 normal, float3 tangent, float3 biNormal, float2 uv)
+{
+    float3 binSpaceNormal = g_NormalTexture.SampleLevel(g_SamplerState, uv, 0.0f).xyz;
+    binSpaceNormal = (binSpaceNormal * 2.0f) - 1.0f;
+    float3 newNormal = tangent * binSpaceNormal.x + biNormal * binSpaceNormal.y + normal * binSpaceNormal.z;
+    return newNormal;
+}
+
+// ベックマン分布を計算する
+float Beckmann(float m, float t)
+{
+    float t2 = t * t;
+    float t4 = t * t * t * t;
+    float m2 = m * m;
+    float D = 1.0f / (4.0f * m2 * t4);
+    D *= exp((-1.0f / m2) * (1.0f - t2) / t2);
+    return D;
+}
+
+// フレネルを計算。Schlick近似を使用
+float SpcFresnel(float f0, float u)
+{
+    return f0 + (1 - f0) * pow(1 - u, 5);
+}
+
+// Cook-Torranceモデルの鏡面反射を計算
+float CookTorranceSpecular(float3 L, float3 V, float3 N, float metallic)
+{
+    float microfacet = 0.76f;
     
-//}
+    // 金属度を垂直入射のときのフレネル反射率として扱う
+    // 金属度が高いほどフレネル反射は大きくなる
+    float f0 = metallic;
+    
+    /// ライトに向かうベクトルと視線に向かうベクトルのハーフベクトル
+    float3 H = normalize(L + V);
+    
+    // 各種ベクトルがどれくらい似ているかを内積を利用して求める
+    float NdotH = saturate(dot(N, H));
+    float VdotH = saturate(dot(V, H));
+    float NdotL = saturate(dot(N, L));
+    float NdotV = saturate(dot(N, V));
+
+    // D項をベックマン分布を用いて計算する
+    float D = Beckmann(microfacet, NdotH);
+    
+    float F = SpcFresnel(f0, VdotH);
+    
+    float G = min(1.0f, min(2 * NdotH * NdotV / VdotH, 2 * NdotH * NdotL / VdotH));
+    
+    float m = PI * NdotV * NdotH;
+    
+    return max(F * D * G / m, 0.0f);        
+}
+
+
+// N : ノーマル
+// L : 光源に向かうベクトル direction?
+// V : 視線に向かうベクトル eyevec
+float CalcDiffuseFromFresnel(float3 N, float3 L, float3 V)
+{
+    float dotNL = saturate(dot(N, L));
+    
+    float dotNV = saturate(dot(N, V));
+    
+    return (dotNL * dotNV);
+
+}
 
 void main(in PS_IN In, out float4 outDiffuse : SV_Target)
 {
     //  ピクセルの法線を正規化
-    float3 normal = normalize(In.Normal);
-    normal.x = -g_NormalTexture.Sample(g_SamplerState, In.TexCoord).r;
-    normal.y = g_NormalTexture.Sample(g_SamplerState, In.TexCoord).b;
-    normal.z = g_NormalTexture.Sample(g_SamplerState, In.TexCoord).g;
+    float3 normal = GetNormal(In.Normal.xyz, In.tangent, In.biNormal, In.TexCoord);
+    //normal.x = -g_NormalTexture.Sample(g_SamplerState, In.TexCoord).r;
+    //normal.y = g_NormalTexture.Sample(g_SamplerState, In.TexCoord).b;
+    //normal.z = g_NormalTexture.Sample(g_SamplerState, In.TexCoord).g;
     
     normal = normal * 2.0f - 0.5f;
     normal = normalize(normal);
@@ -31,12 +95,12 @@ void main(in PS_IN In, out float4 outDiffuse : SV_Target)
     
     normal = In.tangent * localNormal.x + In.biNormal * localNormal.y + normal * localNormal.z;
     
-    float4 ratio = g_PixelNoiseTexture.Sample(g_SamplerState, In.TexCoord);
-    float4 diffuse = g_Texture.Sample(g_SamplerState, In.TexCoord);
-    float4 diffuse2 = g_Texture2.Sample(g_SamplerState, In.TexCoord);
-    float4 resultDiffuse = lerp(diffuse, diffuse2, ratio.r);
+    //float4 ratio = g_PixelNoiseTexture.Sample(g_SamplerState, In.TexCoord);
+    //float4 diffuse = g_Texture.Sample(g_SamplerState, In.TexCoord);
+    //float4 diffuse2 = g_Texture2.Sample(g_SamplerState, In.TexCoord);
+    //float4 resultDiffuse = lerp(diffuse, diffuse2, ratio.r);
     
-    float4 albedoColor = diffuse;
+    float4 albedoColor = g_Texture.Sample(g_SamplerState, In.TexCoord);
     
     float3 specColor = albedoColor;
     
@@ -44,8 +108,25 @@ void main(in PS_IN In, out float4 outDiffuse : SV_Target)
     
     float smooth = 0.5f;
     
+    float toEye = normalize(eyePos.xyz - In.WorldPosition.xyz);
+    float diffuseFromFresnel = CalcDiffuseFromFresnel(normal, -dirDirection, toEye);
+    
+    // 正規化Lambert拡散反射
+    float NdotL = saturate(dot(normal, -dirDirection));
+    float3 lambertDiffuse = dirDirection * NdotL / PI;
+    
+    // 最終的な拡散反射光を計算する
+    float3 diffuse = albedoColor.rgb * diffuseFromFresnel * lambertDiffuse;
+    
+    // Cook-Torranceモデルを利用した鏡面反射率を計算する
+    float3 spec = CookTorranceSpecular(
+    -dirDirection, toEye, normal, smooth) * dirColor;
+    
+    spec *= lerp(float3(1.0, 1.0f, 1.0f), specColor, metalic);
+    
+    
     //  ライト計算
-    float light = saturate(dot(In.Normal.xyz, -dirDirection)) / 3.1415926f;
+    float light = saturate(dot(In.Normal.xyz, -dirDirection)) / PI + diffuseFromFresnel;
     
     //  カメラからのベクトル
     float3 eyevec = In.WorldPosition.xyz - eyePos.xyz;
@@ -53,39 +134,15 @@ void main(in PS_IN In, out float4 outDiffuse : SV_Target)
     
     
     
-    //  反射ベクトル
-    float3 refv = reflect(dirDirection.xyz, normal.xyz);
-    refv = normalize(refv);  
-    
-    //  スペキュラー計算
-    float specular = -dot(eyevec,refv);
-    specular = saturate(specular);    
-    specular = pow(specular, 30.0f);
-    
-    //  高さで色変える。
-    float color_height_diff = In.WorldPosition.y / 5.0f;
-   
-    color_height_diff = pow(frac(color_height_diff), 4.0f);
-    
-    color_height_diff -= 1.0f - In.WorldPosition.y / 10.0f;
-    color_height_diff = saturate(color_height_diff);
-
-    
-    float rim = dot(eyevec, normal.xyz);        
-    rim = 1.0f - abs(rim);
-    rim = pow(rim, 10) * 2.0f;
-    rim = saturate(rim);
 
 //  Diffuse設定            
-    outDiffuse = resultDiffuse;
-       
-    outDiffuse.rgb *= light;
-    outDiffuse.a *= In.Diffuse.a;
+    outDiffuse.rgb = diffuse * (1.0f - smooth) + spec;
+    
+    
+    //outDiffuse.rgb *= light;
+    outDiffuse.a = In.Diffuse.a;
 
-    outDiffuse.rgb += specular;
-    outDiffuse.b += color_height_diff;
-       
-    outDiffuse.b += rim;    
-    outDiffuse.rgb += ambientLight * 1.2f;
+
+    outDiffuse.rgb += ambientLight * 1.2f * albedoColor;
     
 }
