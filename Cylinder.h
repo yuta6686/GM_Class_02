0c1b2cd57aab5615e1be25f6fe48f0150dc2e693
yuta6686 <yuta20010614@gmail.com>
2022-12-27 12:23:22 +0900

    シリンダー.h追加

--- Cylinder.h
+++ Cylinder.h
@@ -3,13 +3,7 @@
 class Cylinder :
     public ComponentObject
 {
-private:
-    std::shared_ptr<Model> m_Model;
-
-    std::shared_ptr <VertexShader> m_VertexShader;
-    std::shared_ptr<PixelShader> m_PixelShader;
 public:
-
 
     // GameObject ‚ğ‰î‚µ‚ÄŒp³‚³‚ê‚Ü‚µ‚½
     virtual void Init() override;
