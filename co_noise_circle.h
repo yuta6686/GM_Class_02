a726e323e7a4b0f400545585657d5b6d80a8b913
yuta6686 <yuta20010614@gmail.com>
2023-01-09 12:08:04 +0900

    https://yuta6686.atlassian.net/browse/AS-9 co_noise_circle.h修正

--- co_noise_circle.h
+++ co_noise_circle.h
@@ -13,13 +13,7 @@
 #include "component_object.h"
 class CO_NoiseCircle :
     public ComponentObject
-{
-	ID3D11Buffer* m_VertexBuffer = NULL;
-
-	std::shared_ptr<Texture> m_Texture;
-
-	std::shared_ptr <VertexShader> m_VertexShader;
-	std::shared_ptr<PixelShader> m_PixelShader;
+{	
 public:
 	void InitInternal()override;
 	void UninitInternal()override;
